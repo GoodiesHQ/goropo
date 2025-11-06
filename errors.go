6386a935b66aba904ae3ad8ba74b8d9d0353e6ad
GoodiesHQ <GoodiesHQ>
2025-11-05 17:21:50 -0800

    Created proper nested errors

--- errors.go
+++ errors.go
@@ -1,6 +1,9 @@
 package goropo
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 var (
 	ErrPoolClosed    = errors.New("pool is closed, task rejected")     // pool has been closed, no new tasks can be submitted
@@ -8,3 +11,46 @@ var (
 	ErrPoolQueueFull = errors.New("pool queue is full, task rejected") // task queue is full, cannot accept new tasks (only used for TrySubmit)
 	ErrTaskPanicked  = errors.New("task panicked during execution")    // task panicked during execution
 )
+
+type TaskPanicError struct {
+	Recovered any
+	Cause     error
+}
+
+func (e *TaskPanicError) Error() string {
+	if e.Cause != nil {
+		return fmt.Sprintf("%v: %v", ErrTaskPanicked, e.Cause)
+	}
+	return fmt.Sprintf("%v: %v", ErrTaskPanicked, e.Recovered)
+}
+
+func (e *TaskPanicError) Unwrap() []error {
+	if e.Cause != nil {
+		return []error{ErrTaskPanicked, e.Cause}
+	}
+	return []error{ErrTaskPanicked}
+}
+
+func IsTaskPanickedError(err error) bool {
+	return errors.Is(err, ErrTaskPanicked)
+}
+
+func AsTaskPanickedError(err error) (*TaskPanicError, bool) {
+	var target *TaskPanicError
+	if errors.As(err, &target) {
+		return target, true
+	}
+	return nil, false
+}
+
+func NewTaskPanicError(recovered any) *TaskPanicError {
+	var cause error
+	if err, ok := recovered.(error); ok {
+		cause = err
+	}
+
+	return &TaskPanicError{
+		Recovered: recovered,
+		Cause:     cause,
+	}
+}
