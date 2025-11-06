4a38d19d029b333ca255cccec9747c880d653188
GoodiesHQ <GoodiesHQ>
2025-11-05 20:48:45 -0800

    Removed locker from public

--- locker.go
+++ locker.go
@@ -6,9 +6,9 @@ import (
 
 // Locker locks the given sync.Locker and returns a function that unlocks it.
 // usage: defer Locker(&mu)()
-func Locker(mu sync.Locker) func() {
-	mu.Lock()
+func Locker(l sync.Locker) func() {
+	l.Lock()
 	return func() {
-		mu.Unlock()
+		l.Unlock()
 	}
 }
